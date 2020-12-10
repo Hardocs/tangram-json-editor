748ca3af37f02b3d8f7194f64812b0650aec9fe9
Narration SD <narrationsd@gmail.com>
2020-12-09 21:01:42 -0800

    this is the first partially operational stage of converting to an npm package version of tangram-js/json-editor, for the Hardocs project. At this point the module can be made to function, but is missing most of its important UI view. We've yet to see that the editor will suit our desktop intention, but with those parts added, experience can begin.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import JsonEditor from './components/JsonEditor.vue'
+import JsonEditor from './App'
+// import JsonView from './JsonView.vue'
+// import ArrayView from './ArrayView.vue'
+// import draggable from 'vuedraggable'
+import PackageFile from '../package.json'
+// import TempInsert from './TemplateInsert.vue';
+
+const VERSION = PackageFile.version
+
+const install = (Vue) => {
+  if (install.installed) return
+
+  // Vue.component('draggable', draggable)
+  // Vue.component('App', App)
+  Vue.component('JsonEditor', JsonEditor)
+  // Vue.component('json-view', JsonView)
+  // Vue.component('array-view', ArrayView)
+}
+
+export default install
+
+export const components = {
+  JsonEditor,
+  VERSION
+}
